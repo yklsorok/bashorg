bc2267ee0e89910a13714f222fe0bc576395a6eb
yklsorok <yklsorok@gmail.com>
2013-06-07 19:56:11 +0300

    Updated setup-bashorg.sh, make more automatic, tested on Lubuntu

--- setup-bashorg.sh
+++ setup-bashorg.sh
@@ -1,13 +1,25 @@
 # install fortune:
 sudo apt-get install fortunes fortune-mod fortunes-min fortunes-ru
 
-# make sure you have configured cpan with root privileges to install modules. if no, configure:
-perl -MCPAN -e o conf make_install_make_command "sudo make"
-perl -MCPAN -e o conf mbuild_install_build_command "sudo ./Build"
-perl -MCPAN -e o conf commit
+if [ -f ~/.cpan/CPAN/MyConfig.pm  ]; then
+  sed -i "/make_install_arg/i \
+  \ \ \'make_install_make_command\' \=\> q[sudo make]," ~/.cpan/CPAN/MyConfig.pm
+  sed -i 's/.\/Build/sudo .\/Build/g' ~/.cpan/CPAN/MyConfig.pm
+else echo "Please, configure cpan by running it, then run the script once again"
+fi
+
+# of configure cpan manually
+# run the commands in the cpan shell
+
+# o conf make_install_make_command "sudo make"
+# o conf mbuild_install_build_command "sudo ./Build"
+# o conf commit
 
 # install necessary Perl modules (LWP::Simple and XML::RSS::Parser::Lite) by cpan:
 perl -MCPAN -e "install LWP::Simple"
+perl -MCPAN -e "install XMLRPC::Lite"
+perl -MCPAN -e "force install SOAP::Lite"
+perl -MCPAN -e "install XML::Parser::Lite"
 perl -MCPAN -e "install XML::RSS::Parser::Lite"
 
 # make Perl script which downloads bash.org.ru cites executable:
