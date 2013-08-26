b214d2fcf7a027449ea07a69d72805eea4267974
Anton Iershov <yklsorok@gmail.com>
2013-08-26 22:28:53 +0300

    fortunes file will be updated every 12 hours

--- bashorg.sh
+++ bashorg.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-if [ ! -f ~/bashorg/bashorg.dat ]; then
-  ~/bashorg/bashorg.pl > ~/bashorg/bashorg && strfile -s ~/bashorg/bashorg ~/bashorg/bashorg.dat
+if [ -f ~/bashorg/bashorg.dat ];then
+    DELTA=$(expr `date +%s` - `stat -c %Y ~/bashorg/bashorg.dat`)
+else
+    DELTA=0
+fi
+if [ $DELTA == 0 ] || [ $DELTA \> 43200 ];then
+    ~/bashorg/bashorg.pl > ~/bashorg/bashorg && strfile -s ~/bashorg/bashorg ~/bashorg/bashorg.dat
 fi
